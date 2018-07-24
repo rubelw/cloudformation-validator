19640193dd61f70c712034f6f1cae14d3746b4f1
Will Rubel <rubelwi@Wills-MacBook-Pro.local>
2018-07-23 20:19:18 -0500

    Fix clean.sh

--- clean.sh
+++ clean.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd $(dirname ${0})
-rm -rf cfn_validator.egg-info/ build/ dist/
+rm -rf cloudformation_validator.egg-info/ build/ dist/
 find . -name .ropeproject -type d | xargs rm -rf
 find . -name "*.pyc" -type f | xargs rm -f
 rm -rf build
